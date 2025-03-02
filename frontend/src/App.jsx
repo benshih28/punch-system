2b87440ffdc41ea53d0fac96fe3860a53d288dbb
yourName <a9900522@gmail.com>
2025-03-02 14:26:39 +0800

    feat:add LoginPage
    add pages/LoginPage.jsx
    update App.jsx
    
    有安裝 @mui/material，但 @mui/icons-material 需要另外安裝
    ->npm install @mui/icons-material

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage"; // 引入登入頁面
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* 預設導向至登入頁面 */}
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        
+        {/* 登入頁面 */}
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
