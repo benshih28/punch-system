30673c3c22d9bd81c1d1a3a139e20a4e352790d1
Cryssstalllll <crystal20228@gmail.com>
2025-03-10 14:21:32 +0800

    fix:password rules
    1. password rules
    origin:min(9)
    new:min(8), symbols ->expect the quote
    2. UserController.php
    new_password can not be same with the old_password

--- backendApi/app/Http/Controllers/UserController.php
+++ backendApi/app/Http/Controllers/UserController.php
@@ -24,34 +24,38 @@ class UserController extends Controller
             'new_password' => [
                 'nullable',
                 'confirmed',
-                Password::min(9),
-                // ->mixedCase()->numbers()],
-                'regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{9,}$/'
+                Password::min(8),
+                'regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!#$%&*+\-.\/]).{8,}$/'
             ],
             'new_password_confirmation' => 'required_with:new_password',
             'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
         ], [
             //檢查新密碼跟確認新密碼是否相符
             'new_password.confirmed' => '新密碼與確認新密碼不一致',
-            'new_password.min' => '新密碼至少需要 9 個字元(包含大小寫字母、數字和特殊符號)',
-            'new_password.regex' => '新密碼至少需要 9 個字元(包含大小寫字母、數字和特殊符號)',
+            'new_password.min' => '新密碼至少需要 8 個字元(包含大小寫字母、數字和特殊符號)',
+            'new_password.regex' => '新密碼至少需要 8 個字元(包含大小寫字母、數字和特殊符號)',
             'new_password_confirmation.required_with' => '請輸入確認密碼'
         ]);
 
 
-        // 更新密碼(跟檢查舊密碼是否正確）
+        // 新密碼
         if ($request->filled('new_password')) {
+            // 1.檢查是否輸入舊密碼
             if (!$request->filled('old_password') || !Hash::check($request->old_password, $user->password)) {
                 return response()->json(['message' => '舊密碼錯誤'], 403);
             }
+            // 2.防止新密碼與舊密碼相同
+            if (Hash::check($request->new_password, $user->password)) {
+                return response()->json(['message' => '新密碼不能與舊密碼相同'], 403);
+            }
+            // 3.更新密碼
             $user->password = Hash::make($request->new_password);
         }
 
-
-        // ✅ Debug 記錄
+        // Debug 記錄
         // Log::info('User profile updated: ' . $user->id);
 
-        // ✅ 透過 FileController 上傳大頭貼
+        // 透過 FileController 上傳大頭貼
         // 如果有上傳新大頭貼，直接呼叫 FileController@uploadAvatar 方法
         if ($request->hasFile('avatar')) {
             $fileController = app(FileController::class);
@@ -68,6 +72,5 @@ class UserController extends Controller
         $user->save();
 
         return response()->json(['message' => '個人資料已更新', 'avatar' => $avatarUrl]);
-
     }
 }
