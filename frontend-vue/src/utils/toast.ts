fbe23384924e72b0f560686d79be5f1f026290af
benshih <32964867+benshih28@users.noreply.github.com>
2025-05-23 16:55:52 +0800

    fix: clean unused document

--- frontend-vue/src/utils/toast.ts
+++
@@ -1,6 +0,0 @@
-import { useToast } from "vue-toastification";
-const toast = useToast();
-
-export function showToast(message: string, type: "info" | "success" | "error" = "info") {
-  toast[type](message);            // ex: toast.error('登入失敗')
-}
